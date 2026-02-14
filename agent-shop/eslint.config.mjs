e6b525bf11e1e44c38da027b7615c7010b28b984
Fidel <trabajosfidel4@gmail.com>
2026-02-13 23:12:46 -0300

    fix: ignore contract folders in build

--- agent-shop/eslint.config.mjs
+++ agent-shop/eslint.config.mjs
@@ -12,6 +12,11 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "lib/**",
+    "contracts/**",
+    "broadcast/**",
+    "cache/**",
+    "hardhat.config.ts"
   ]),
 ]);
 
